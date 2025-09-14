12e4e8bea70b04304d72eaf5ae7e51ccc0a2b3e1
inokawa <48897392+inokawa@users.noreply.github.com>
2025-09-15 02:59:37 +0900

    wip

--- src/painter/canvas.ts
+++ src/painter/canvas.ts
@@ -8,27 +8,40 @@ export const canvasPainter = (
 
   const bgColor = options.bgColor ?? "#000000";
 
+  const measureText = (
+    text: string,
+    font: string
+  ): { width: number; height: number } => {
+    ctx.font = font;
+    const metrics = ctx.measureText(text);
+
+    return {
+      width: metrics.width,
+      height:
+        metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent,
+    };
+  };
+
   return {
     size: (width, height, dpr) => {
       canvas.width = width * dpr;
       canvas.height = height * dpr;
       ctx.scale(dpr, dpr);
     },
+    measureText,
     draw: (views) => {
       ctx.fillStyle = bgColor;
       ctx.fillRect(0, 0, canvas.width, canvas.height);
 
       for (const view of views) {
         if ("text" in view) {
-          ctx.font = view.fontSize + "px sans-serif";
-          ctx.fillStyle = view.color;
+          const font = view.fontSize + "px sans-serif";
           // Because by default in Canvas API Y is the baseline of the
           // text, we need to calculate the height of the text and then
           // offset it by that amount.
-          const metrics = ctx.measureText(view.text);
-          const height =
-            metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
-
+          const { height } = measureText(view.text, font);
+          ctx.font = font;
+          ctx.fillStyle = view.color;
           ctx.fillText(view.text, view.x, view.y + height);
         } else if (view.backgroundColor !== "transparent") {
           ctx.fillStyle = view.backgroundColor;
--- src/painter/index.ts
+++ src/painter/index.ts
@@ -2,5 +2,9 @@ import { Draw } from "../types";
 
 export interface Painter {
   size: (width: number, height: number, dpr: number) => void;
+  measureText: (
+    text: string,
+    font: string
+  ) => { width: number; height: number };
   draw: (views: readonly Draw[]) => void;
 }
